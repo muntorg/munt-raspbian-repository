52b520da196ff9d2783d702fbc01da0641312ede
Malcolm MacLeod <malcolm@gulden.com>
2019-10-24 22:49:22 +0200

    script fixes

--- tools/fulldatadirwipe.sh
+++ tools/fulldatadirwipe.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Kill the app
-killall -9 GuldenD
+killall -9 GuldenD || true
 
 # Wipe all important parts of datadir that can be refenerated (exclude wallet/logs etc.)
 rm -rf /opt/gulden/datadir/autocheckpoints || true
@@ -9,10 +9,10 @@ rm -rf /opt/gulden/datadir/banlist.dat || true
 rm -rf /opt/gulden/datadir/peers.dat || true
 rm -rf /opt/gulden/datadir/db.log || true
 rm -rf /opt/gulden/datadir/mempool.dat || true
-rm -rf /opt/Gulden/datadir/blocks || true
-rm -rf /opt/Gulden/datadir/database || true
-rm -rf /opt/Gulden/datadir/chainstate || true
-rm -rf /opt/Gulden/datadir/witstate || true
+rm -rf /opt/gulden/datadir/blocks || true
+rm -rf /opt/gulden/datadir/database || true
+rm -rf /opt/gulden/datadir/chainstate || true
+rm -rf /opt/gulden/datadir/witstate || true
 
 #restart
 /opt/gulden/guldenstart.sh &
--- tools/partialdatadirwipe.sh
+++ tools/partialdatadirwipe.sh
@@ -1,11 +1,11 @@
 #!bin/bash
 
 # Try close gracefully
-killall -15 GuldenD
+killall -15 GuldenD || true
 sleep 10
 
 # Kill if that failed
-killall -9 GuldenD
+killall -9 GuldenD || true
 
 # Wipe certain parts of datadir
 rm -rf /opt/gulden/datadir/autocheckpoints || true
